5b9479af6ae3277986f9e0a651d1a591c4399309
Ian McPhail <ian.mcphail@thinkcompany.com>
2018-10-23 21:15:56 -0400

    Add initial components with mock data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
 
+import ShowQueue from './ShowQueue';
+import ShowTile from './ShowTile';
+
+import MOCK_DATA from 'data/feed.json';
+// GET http://api.tvmaze.com/shows
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      loaded: false
+      loaded: false,
+      shows: [],
+      selectedShows: []
     };
   }
 
+  componentDidMount() {
+    if (MOCK_DATA.length) {
+      this.setState({
+        loaded: true,
+        shows: MOCK_DATA
+      });
+    }
+  }
+
   get queue() {
-    return null;
+    const { selectedShows } = this.state;
+
+    if (!selectedShows.length) {
+      return null;
+    }
+
+    return (
+      <ShowQueue
+        shows={selectedShows}
+        onSelected={id => console.log(`SELECTED: ${id}`)}
+      />
+    );
   }
 
   get shows() {
-    return null;
+    const { shows } = this.state;
+
+    if (!shows.length) {
+      return null;
+    }
+
+    return (
+      <div className="my-shows">
+        {shows.map(show => (
+          <ShowTile
+            key={show.id}
+            onSelected={id => console.log(`SELECTED: ${id}`)}
+            {...show}
+          />
+        ))}
+      </div>
+    );
   }
 
   get spinner() {
