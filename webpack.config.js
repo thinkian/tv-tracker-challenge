e4bb3ec20ab8f33f79f1ad015e8d215ca0458860
Ian McPhail <ian.mcphail@thinkcompany.com>
2020-01-22 10:04:04 -0500

    Update dependencies and Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const path = require('path');
-const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   entry: {
-    main: ['babel-polyfill', './src/index.js']
+    main: ["babel-polyfill", "./src/index.js"]
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     disableHostCheck: true,
-    host: 'localhost',
+    host: "localhost",
     hot: true,
     open: true,
-    openPage: '',
+    openPage: "",
     port: 8000
   },
   module: {
@@ -26,52 +26,55 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['env', 'react']
+            presets: ["@babel/preset-env", "@babel/preset-react"]
           }
         }
       },
       {
         test: /\.(sa|sc|c)ss$/,
         exclude: /node_modules/,
-        use: ['extracted-loader'].concat(
-          ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: ['css-loader', 'sass-loader']
-          })
-        )
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader",
+          "sass-loader"
+        ]
       }
     ]
   },
   node: {
-    dns: 'empty',
+    dns: "empty",
     global: true,
-    fs: 'empty',
-    net: 'empty',
+    fs: "empty",
+    net: "empty",
     process: true
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(['dist'], {
+    new CleanWebpackPlugin({
       verbose: false
     }),
-    new ExtractTextPlugin('styles.css'),
-    new HtmlWebpackPlugin({
-      title: 'TV Tracker Challenge',
-      template: './src/index.html'
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
     }),
-    new UglifyJsPlugin({
+    new HtmlWebpackPlugin({
+      title: "TV Tracker Challenge",
+      template: "./src/index.html"
+    }),
+    new TerserPlugin({
       sourceMap: true
     })
   ],
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-    modules: [path.resolve('./src'), path.resolve('./node_modules')]
+    extensions: [".js", ".jsx", ".json"],
+    modules: [path.resolve("./src"), path.resolve("./node_modules")]
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
   }
 };
